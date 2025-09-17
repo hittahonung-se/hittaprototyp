49a29df3722ae25cd91bbbf80e7281397d8917ef
Nina Wängberg <nina.e.s.wangberg@gmail.com>
2025-09-17 11:12:00 +0200

    Add a list of tags

---
+++ tags.js
@@ -0,0 +1,68 @@
+tags = [
+    {
+        name: "Ljunghonung",
+        description: "Honung från ljung",
+        category: "type",
+    },
+    {
+        name: "Maskroshonung",
+        description: "Honung från maskrosor",
+        category: "type",
+    },
+    {
+        name: "Rökt honung",
+        description: "Honung som är rökt",
+        category: "type",
+    },
+    {
+        name: "Trädsgårdshonung",
+        description: "Honung från blandade växter",
+        category: "type",
+    },
+    {
+        name: "Bladhonung",
+        description: "Honung från honungsdagg",
+        category: "type",
+    },
+    {
+        name: "Kakhonung",
+        description: "Honung i vaxkaka",
+        category: "type",
+    },
+    {
+        name: "Gårdsbutik",
+        description: "Försäljning på plats i bod eller skåp",
+        category: "sales_method",
+    },
+    {
+        name: "Andelsbiodling",
+        description: "Säljer andelar av sin honungsskörd",
+        category: "sales_method",
+    },
+    {
+        name: "Marknadsförsäljning",
+        description: "Säljer på marknader",
+        category: "sales_method",
+    },
+    {
+        name: "Kurser och föreläsningar",
+        description: "Håller kurser och föreläsningar och biodling",
+        category: "other",
+    },
+    {
+        name: "Studiebesök",
+        description: "Erbjuder studiebesök i biodlingen",
+        category: "other",
+    },
+
+    {
+        name: "Hobbybiodlare",
+        description: "Biodlar på hobbynivå",
+        category: "other",
+    },
+    {
+        name: "Yrkesbiodlare",
+        description: "Livnär sig helt eller delvis på biodling",
+        category: "other",
+    },
+]
